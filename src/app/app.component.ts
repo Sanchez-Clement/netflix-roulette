7490a7026f31c255f8386bd739dc21e954b028bb
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-09-01 09:26:47 +0200

    add two component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+// import { Injectable } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+
+  styleUrls: ['./app.component.css'],
+
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit{
+  title = 'Star Wars';
+  results: any;
+  resultatEnvoi : any;
+  resultatEnvoiNon : any;
+  urlEnvoi : any;
+  resultatDetail : any;
+
+
+// Inject HttpClient into your component or service.
+constructor(private http: HttpClient) {}
+ngOnInit (){
+
+
+};
+
+Send(test) {
+  this.resultatEnvoi = test.replace(/ /g,"%20");
+  this.http.get('https://netflixroulette.net/api/api.php?title=' + this.resultatEnvoi).subscribe(data => {
+    // Read the result field from the JSON response.
+    this.results = data;
+    this.urlEnvoi =  test;
+    console.table(data)
+
+
+  });
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+
+import {HttpClientModule} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
